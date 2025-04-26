1c8e6185eaa98ad3a5fe6c9c6d8cd10c0a047e1c
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2025-04-26 20:04:22 +0400

    initial commit

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,112 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+const Register = () => {
+  const router = useRouter();
+  return (
+    <div className="bg-black w-full h-screen">
+      <div className="relative flex flex-col justify-center items-center">
+        <img
+          src="/images/login-bg.jpg"
+          alt="login-bg"
+          className="w-full h-[750px] object-cover"
+        />
+        <div className="bg-[#000000B2] opacity-[50%] w-full h-[750px] absolute"></div>
+        <div className="w-full absolute top-[25px] pl-[160px]">
+          <img
+            src="/icons/netflix-logo.png"
+            alt="netflix-logo"
+            className="w-[150px] h-[40px]"
+          />
+        </div>
+        <div className="bg-[#000000B2] absolute top-0 mt-[110px] px-[70px] py-[50px]">
+          <h2 className="text-[32px] font-bold leading-[100%] text-white">
+            Sign Up
+          </h2>
+          <div className="flex flex-col">
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-[320px] mt-[30px] border-[1px] bg-[#161616B2] border-[#808080B2] text-white text-[16px] leading-[24px] font-normal px-[15px] py-[20px] rounded-[5px]"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-[320px] mt-[15px] border-[1px] bg-[#161616B2] border-[#808080B2] text-white text-[16px] leading-[24px] font-normal px-[15px] py-[20px] rounded-[5px]"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-[320px] mt-[15px] border-[1px] bg-[#161616B2] border-[#808080B2] text-white text-[16px] leading-[24px] font-normal px-[15px] py-[20px] rounded-[5px]"
+            />
+          </div>
+          <button className="w-full mt-[15px] bg-[#E50914] rounded-[5px] py-[10px] text-center text-[16px] leading-[16px] font-medium text-white hover:cursor-pointer">
+            Sign Up
+          </button>
+          <div
+            onClick={() => router.push("/login")}
+            className="flex flex-row justify-center mt-[40px] hover:cursor-pointer"
+          >
+            <h3 className="text-[#FFFFFFB2] font-normal text-[16px] leading-[100%]">
+              Already Have an Account?
+            </h3>
+            <h2 className="ml-[5px] font-medium text-[16px] text-white leading-[100%]">
+              Sign in
+            </h2>
+          </div>
+        </div>
+        <div className="w-full absolute bottom-[0px] h-[50px] bg-[#000000B2]"></div>
+      </div>
+      <div className="flex flex-row justify-between bg-[#000000] pt-[50px] px-[170px] pb-[70px]">
+        <div className="flex flex-col">
+          <div className="flex flex-row">
+            <h3 className="text-[16px] leading-[100%] font-normal text-[#FFFFFFB2]">
+              Questions? Call{" "}
+            </h3>
+            <h3 className="text-[16px] leading-[100%] font-normal text-[#FFFFFFB2]">
+              1-844-505-2993
+            </h3>
+          </div>
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            FAQ
+          </h3>
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Privacy
+          </h3>
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Ad Choices
+          </h3>
+        </div>
+
+        <div className="mt-[20px] flex flex-col">
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Help Center
+          </h3>
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Cookie Preferences
+          </h3>
+        </div>
+
+        <div className="mt-[20px] flex flex-col">
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Netflix Shop
+          </h3>
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Corporate Information
+          </h3>
+        </div>
+
+        <div className="mt-[20px] flex flex-col">
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Terms of Use
+          </h3>
+          <h3 className="text-[14px] mt-[25px] underline leading-[100%] font-normal text-[#FFFFFFB2]">
+            Do Not Sell or Share My Personal Information
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,186 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
+import { describe } from "node:test";
 
-export default function Home() {
+const Landing = () => {
+  const cards = [
+    {
+      id: 1,
+      title: "Enjoy on your TV",
+      description:
+        "Watch on Smart TVs,Playstation, Xbox,Chromecast, Apple TV, Blu-ray players, and more.",
+      logoPath: "/icons/television.png",
+    },
+    {
+      id: 2,
+      title: "Download your shows to watch offline",
+      description:
+        "Save your favorites easily and always have something to watch.",
+      logoPath: "/icons/download.png",
+    },
+    {
+      id: 3,
+      title: "Watch everywhere",
+      description:
+        "Stream unlimited movies and TV shows on your phone,tablet, laptop, and TV.",
+      logoPath: "/icons/watch.png",
+    },
+    {
+      id: 4,
+      title: "Create profiles for kids",
+      description:
+        "Send kids on adventures with their favorite characters in a space made just for them — free with your membership.",
+      logoPath: "/icons/profiles.png",
+    },
+  ];
+
+  const questions = [
+    "What is Netflix?",
+    "How much does Netflix cost?",
+    "Where can I watch?",
+    "How do I cancel?",
+    "What can I watch on Netflix?",
+    "Is Netflix good for kids?",
+    "Why am I seeing this language?",
+  ];
+
+  const router = useRouter();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="bg-black w-full h-screen">
+      <div className="relative flex flex-col justify-center items-center">
+        <img
+          src="/images/Background.jpg"
+          alt="Background"
+          className="w-full h-[700px] object-cover"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="bg-[#000000B2] w-full h-[700px] absolute"></div>
+        <div className="w-full flex flex-row justify-between absolute top-[20px] px-[140px]">
+          <img
+            src="/icons/netflix-logo.png"
+            alt="netflix-logo"
+            className="w-[160px] h-[50px]"
+          />
+          <div className="mt-[10px] flex flex-row">
+            <button
+              onClick={() => router.push("/login")}
+              className="bg-[#E50914] rounded-[5px] py-[7px] px-[20px] text-[14px] leading-[20px] font-semibold text-white hover:cursor-pointer"
+            >
+              Sign In
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="absolute top-[0px] mt-[210px] w-[700px] flex flex-col items-center">
+          <div className="w-[610px]">
+            <h1 className="font-bold text-[60px] leading-[65px] text-white text-center">
+              Unlimited movies, TV shows, and more
+            </h1>
+            <h2 className="font-bold mt-[20px] text-[25px] leading-[30px] text-white text-center">
+              Starts at $6.99. Cancel anytime.
+            </h2>
+          </div>
+          <div className="w-full">
+            <h2 className="text-[22px] text-white leading-[24px] font-normal mt-[30px] text-center">
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h2>
+            <div className="w-full flex flex-row mt-[20px]">
+              <input
+                placeholder="Enter email"
+                className="w-[490px] border-[1px] border-[#A1A1AA] text-[#9CA3AF] p-[15px] rounded-[5px]"
+              />
+              <button className="flex flex-row ml-[10px] bg-[#E50914] rounded-[5px] py-[13px] px-[25px] text-[20px] leading-[28px] font-semibold text-white hover:cursor-pointer">
+                Get Started
+                <img
+                  className="w-[30px] h-[30px] ml-[5px]"
+                  src="/icons/right-arrow.svg"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+        <img
+          src="/images/round-image.png"
+          className="w-full h-fit absolute bottom-[0px]"
+        />
+      </div>
+      <div className="bg-black pt-[80px] pb-[40px] px-[140px]">
+        <h3 className="font-semibold text-[24px] leading-[32px] text-white">
+          Trending Now
+        </h3>
+
+        <h3 className="text-[24px] leading-[36px] font-bold mt-[80px] text-white">
+          More Reasons to Join
+        </h3>
+
+        <div className="grid grid-cols-4 gap-[15px] mt-[20px]">
+          {cards.map((card) => (
+            <div
+              key={card.id}
+              className="bg-[#192247] rounded-[10px] flex flex-col justify-between px-[15px] py-[20px]"
+            >
+              <h3 className="font-bold text-[24px] leading-[32px] text-white text-start">
+                {card.title}
+              </h3>
+              <p className="text-[#FFFFFFB2] text-[16px] leading-[24px] font-normal text-start mt-[15px]">
+                {card.description}
+              </p>
+              <div className="flex flex-row justify-end">
+                <img
+                  src={card.logoPath}
+                  alt="logo"
+                  className="w-[80px] h-[80px] mt-[20px]"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <h3 className="font-medium text-[26px] leading-[34px] mt-[65px] text-white">
+          Frequently Asked Questions
+        </h3>
+        <div className="mt-[20px]">
+          {questions.map((question, index) => (
+            <div key={index} className="py-[30px] px-[25px] w-full bg-[#2D2D2D] mb-[10px] flex flex-row justify-between items-center">
+              <h2 className="font-normal text-[24px] leading-[36px] text-white">{question}</h2>
+              <img src="/icons/plus.png" alt="plus" className="w-[30px] h-[30px]" />
+            </div>
+          ))}
+        </div>
+        <div className="mt-[150px] flex flex-row justify-between items-center">
+          <div className="flex flex-col">
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline">FAQ</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Legal Notices</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Corporate Information</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Terms of Use</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Privacy</h3>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline">Investor Relations</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Help Center</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Only on Netflix</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Contact Us</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Speed Test</h3>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline">Buy Gift Cards</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Jobs</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Account</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Media Center</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px] w-[220px]">Do Not Sell or Share My Personal Information</h3>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline">Cookie Preferences</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Ways to Watch</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Netflix Shop</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Redeem Gift Cards</h3>
+            <h3 className="font-normal text-[14px] leading-[20px] text-[#FFFFFFB2] underline mt-[10px]">Ad Choices</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Landing;
